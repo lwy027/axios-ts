6abfcc3d6686c65a3529fc36a2085336f47d5e93
liweiye <2759536538@qq.com>
2023-06-27 10:51:56 +0800

    完成全局拦截器

--- src/server/request/index.ts
+++ src/server/request/index.ts
@@ -7,6 +7,19 @@ class WyRequest {
 
   constructor(config: AxiosRequestConfig) {
     this.instance = axios.create(config)
+    //全局拦截器
+    this.instance.interceptors.request.use(config => {
+      console.log("全局的请求拦截器")
+      return config
+    }, err => {
+      return err
+    })
+    this.instance.interceptors.response.use(res => {
+      console.log("全局的响应拦截器")
+      return res.data
+    }, err => {
+      return err
+    })
   }
 
   request(config: AxiosRequestConfig) {
